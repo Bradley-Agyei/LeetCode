a4f8e950b33a50667b2f14b508533d90868752a7
Bradley Agyei <Bradley-Agyei@users.noreply.github.com>
2022-06-16 00:08:02 -0400

    added ListNode for merge sorted lists

--- Python/Linked Lists/Easy/mergeTwoSortedLists.py
+++ Python/Linked Lists/Easy/mergeTwoSortedLists.py
@@ -1,3 +1,8 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next 
+
 class Solution(object):
     def mergeLists(self, list1, list2):
         # init prehead variable 
